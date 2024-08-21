497f61a85e801cabf366ce70836ed287bc531943
Arjun Ghosh <gharjun69@gmail.com>
2024-08-21 17:11:01 +0530

    Update lakeshore.py

--- APSERa Codes/lakeshore_controller/lakeshore.py
+++ APSERa Codes/lakeshore_controller/lakeshore.py
@@ -9,7 +9,7 @@ def initialize_serial(port, baudrate):
         bytesize=serial.SEVENBITS,
         parity=serial.PARITY_ODD,
         stopbits=serial.STOPBITS_ONE,
-        timeout=0  # Set to 0 to manage timeout manually
+        timeout=1  # Set to 0 to manage timeout manually
     )
 
 # Read response with a timeout
@@ -33,42 +33,47 @@ def read_response(serial_port, timeout_period, terminator):
     return None
 
 # Main program loop
-def main():
+def setup():
     # Configuration
-    port = "/dev/ttyUSB4"      # Replace with your serial port
-    baudrate = 9600      # Baud rate
-    timeout = 1         # Timeout in seconds
-    terminator = '\r\n'  # Termination characters (CRLF)
+    port = (input('port: ').strip() or "/dev/ttyUSB4")     # serial port
+    baudrate = (input('baudrate: ').strip() or "9600")       # Baud rate
+
     
     # Initialize the serial port
-    ser = initialize_serial(port, baudrate)
-    
-    print("SERIAL COMMUNICATION PROGRAM")
-    
-    while True:
-        # Get command from user
-        cmd = input("ENTER COMMAND (or EXIT): ").strip().upper()
+    try: 
+        ser = initialize_serial(port, baudrate)
+        print("SERIAL COMMUNICATION SUCCESS")
+    except: 
+        print("SERIAL COMMUNICATION FAILED")
         
-        if cmd == "EXIT":
-            ser.close()  # Close the serial port
-            print("Exiting...")
-            break
-        
-        # Add terminator to command
-        cmd += terminator
-        
-        # Send command to instrument
-        ser.write(cmd.encode())
-        
-        if "?" in cmd:
-            # If query, read response
-            print("Waiting for response...")
-            response = read_response(ser, timeout, terminator)
-            
-            if response:
-                print("RESPONSE:", response)
-            else:
-                print("NO RESPONSE")
+    return ser
 
-if __name__ == "__main__":
-    main()
+
+ser = setup()    
+terminator = '\r\n'  # Termination characters (CRLF)
+read_timeout = 1
+
+while True:
+    # Get command from user
+    cmd = input("ENTER COMMAND (or EXIT): ").strip().upper()
+
+    if cmd == "E":
+        ser.close()  # Close the serial port
+        print("Exiting...")
+        break
+
+    # Add terminator to command
+    cmd += terminator
+
+    # Send command to instrument
+    ser.write(cmd.encode())
+
+    if "?" in cmd:
+        # If query, read response
+        print("Waiting for response...")
+        response = read_response(ser, read_timeout, terminator)
+
+    if response:
+        print("RESPONSE:", response)
+    else:
+        print("NO RESPONSE")
